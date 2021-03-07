5bb36c7831d9bb7db9b60f231093b7098d647126
ShaodongGu <shaodong@uci.edu>
2021-03-07 20:40:18 +0800

    Fix the user input not displaying bug.

--- app/src/main/java/com/example/fitnessguru/HomeFragment.java
+++ app/src/main/java/com/example/fitnessguru/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,21 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button finish = v.findViewById(R.id.finish_button);
+        ProgressBar p1 = v.findViewById(R.id.CaroliesProgressBar);
+        ProgressBar p2 = v.findViewById(R.id.ExerciseMinsProgressBar);
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (p1.getProgress() < 100) {
+                    p1.incrementProgressBy(10);
+                    p2.incrementProgressBy(10);
+                }
+            }
+        });
+
+        return v;
     }
 }
